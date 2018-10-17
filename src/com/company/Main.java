d4856fbf3c1066f918a216a778cfe67c7bee11b5
nazar <nazar.petruk@gmail.com>
2018-10-17 22:48:57 +0300

    try

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a);
+            a = a + a;
+
+        }
+    }
+}
